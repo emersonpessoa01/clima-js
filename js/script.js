c586c746fd853a88804c4f879774b7236470874f
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2023-09-28 17:18:43 -0300

    finished style :sparkles:

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 //API= d0b0e263748d76e177f14c61c484fd74
 
+/* OPEN WAETHER MAP*/
 // https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=d0b0e263748d76e177f14c61c484fd74
+
+/* Country  Flags Icons */
+// https://countryflagicons.com/
+
+//https://www.countryflags.com/
